3439cdad341d1bc46186ed31ada3ca1cb82b2206
ilya <ilya1291@gmail.com>
2016-11-24 14:50:35 +0300

    Добавлено построение модели

--- src/Main.scala
+++ src/Main.scala
@@ -1,33 +1,16 @@
-import ru.ilya.ml.DataFrame
+import ru.ilya.ml.{DataFrame, LinearRegression}
 
 
 object Main {
-
+  //    val humanMetrics: List[DataFrame] = DataFrame.fromCsv("Davis.csv")
 
   def main(args: Array[String]): Unit = {
 
-    // http://www.math-pr.com/exampl_sts3.htm
+    val humanMetrics: List[DataFrame] = DataFrame.fromCsv("Davis.csv")
+//    val humanMetrics: List[DataFrame] = DataFrame.fromCsv("d.csv")
+    val y: (Double) => Double = LinearRegression.buildModel(humanMetrics)
 
-
-//    val humanMetrics: List[DataFrame] = DataFrame.fromCsv("Davis.csv")
-    val humanMetrics: List[DataFrame] = DataFrame.fromCsv("d.csv")
-
-    val sumMetrics = humanMetrics.reduce(_+_)
-
-    val meanX = sumMetrics.x / humanMetrics.size
-    val meanY = sumMetrics.y / humanMetrics.size
-
-    val meanXY = humanMetrics.map(_.multXY()).reduce(_+_) / humanMetrics.size
-    val sqSumXY = humanMetrics.map(_^2).reduce(_+_) / humanMetrics.size - DataFrame(meanX * meanX, meanY * meanY)
-    val rXY = (meanXY - meanX * meanY) / Math.sqrt(sqSumXY.multXY)
-
-    val b = rXY * (Math.sqrt(sqSumXY.y / sqSumXY.x))
-    val a = meanY - (b * meanX)
-
-    println(a)
-    println(b)
-
-
+    println(y(25.9))
 
   }
 }
--- src/ru/ilya/ml/LinearRegression.scala
+++ src/ru/ilya/ml/LinearRegression.scala
@@ -1,10 +1,23 @@
 package ru.ilya.ml
 
 
-
 object LinearRegression {
 
-  def buildRegressionEquation(dataFrame: DataFrame): (Double => Double) = {
+  // http://www.math-pr.com/exampl_sts3.htm
+  def buildModel(dataFrames: List[DataFrame]): (Double => Double) = {
 
+    val sumMetrics = dataFrames.reduce(_ + _)
+
+    val meanX = sumMetrics.x / dataFrames.size
+    val meanY = sumMetrics.y / dataFrames.size
+
+    val meanXY = dataFrames.map(_.multXY()).reduce(_ + _) / dataFrames.size
+    val sqSumXY = dataFrames.map(_ ^ 2).reduce(_ + _) / dataFrames.size - DataFrame(meanX * meanX, meanY * meanY)
+    val rXY = (meanXY - meanX * meanY) / Math.sqrt(sqSumXY.multXY)
+
+    val b = rXY * (Math.sqrt(sqSumXY.y / sqSumXY.x))
+    val a = meanY - (b * meanX)
+
+    x: Double => a + b * x
   }
 }
